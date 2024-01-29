fb0ac1e93bec3aa59ceef44dabc9c81b054cd938
Iryna Romashko <137172921+it-tinyangel@users.noreply.github.com>
2024-01-29 13:02:54 +0200

    Updated js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let arr = [1, 'string', [3, 4], 5, [6, 7]];
 
-const subarrays = arr.filter(item => Array.isArray(item));
+const subArrays = arr.filter(item => Array.isArray(item));
 
-console.log('Subarrays in an array:', subarrays);
+console.log('Subarrays in an array:', subArrays);
